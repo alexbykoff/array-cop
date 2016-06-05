ad36b571d0b47670ec997656caa1793e57c020ee
tomkallen <spbeat@mail.ru>
2016-06-06 01:05:54 +0300

    fix minor codestyles

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ It is just an utility tools that makes life easier.
 - Remove duplicates from an array  
 - Pick one random item from an array in a specific range (or not)  
 
-*ultra-early build. unpublished*  
+*ultra-early dev build. NOT PUBLISHED*  
 
+###Installation###
 `npm install --save array-cop`
+
+###Usage###
+`var array_ = require('array-cop');`
--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 module.exports = {
-        isValid: function(arr) {
-            return (Array.isArray(arr)) ? true : console.log(typeof(arr) + " is not an Array");
-        },
+    isValid: function(arr) {
+        return (Array.isArray(arr)) ? true : console.log(typeof(arr) + " is not an Array");
+    },
 
-        flatten: function(arr) {
+    flatten: function(arr) {
+        // Flattens all the layers of an array.
 
-            // Flattens all the layers of an array.
-            var _ = this;
-            return this.isValid(arr) ? arr.reduce(function(flat, i) {
-                return flat.concat(Array.isArray(i) ? _.flatten(i) : i);
-            }, []) : arr;
-        },
-        dedup: function(arr) {
+        var __ = this;
+        return this.isValid(arr) ? arr.reduce(function(f, i) {
+            return f.concat(Array.isArray(i) ? __.flatten(i) : i);
+        }, []) : arr;
+    },
 
-            // Removes duplicates from an array. Note: items in nested arrays are not
-            // considered to be duplicates. If you want to remove duplicates from
-            // the sub-arrays as well then apply flatten() first.
-            if (this.isValid(arr)) {
-                var newArray = [];
-                arr.forEach(function(i) {
-                    if (newArray.indexOf(i) === -1) {
-                        newArray.push(i)
-                    }
-                });
-                return newArray;
-            }
-            return arr;
-        },
-        rand: function(arr, min, max) {
+    dedup: function(arr) {
+        // Removes duplicates from an array. Note: items in nested arrays are not
+        // considered to be duplicates. If you want to remove duplicates from
+        // the sub-arrays as well then apply flatten() first.
 
-            // Picks a random items from an array in a range of min, max.
-            // If no range is provided then array.length is taken
-            // into a consideraton
-            return this.isValid(arr) ? arr[Math.floor(Math.random() * ((max || arr.length) - (min || 0))) + (min || 0)]: arr;
-            }
+        if (this.isValid(arr)) {
+            var newArray = [];
+            arr.forEach(function(i) {
+                if (newArray.indexOf(i) === -1) {
+                    newArray.push(i)
+                }
+            });
+            return newArray;
         }
+        return arr;
+    },
+
+    rand: function(arr, min, max) {
+        // Picks a random items from an array in a range of min, max.
+        // If no range is provided then array.length is taken
+        // into a consideraton
+
+        return this.isValid(arr) ? arr[Math.floor(Math.random() * ((max || arr.length) - (min || 0))) + (min || 0)] : arr;
+    }
+}
