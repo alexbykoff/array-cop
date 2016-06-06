a2daec043d61737105e57a9ffcf62562476ad683
tomkallen <spbeat@mail.ru>
2016-06-06 10:59:38 +0300

    feature: sum() method

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Array Cop#
+#array-cop#
 
 Array cop is a zero-dependant vanilla JS nano-library / npm module that deals with arrays.
 It is just an utility tools that makes life easier.
@@ -7,6 +7,7 @@ It is just an utility tools that makes life easier.
 - Flatten an array
 - Remove duplicates from an array  
 - Pick one random item from an array in a specific range (or not)  
+- Returns the sum of all numbers in an array no matter how deeply nested they are ignoring all other types of items
 
 *ultra-early dev build. NOT PUBLISHED TO NPM*  
 
--- src/array-cop.js
+++ src/array-cop.js
@@ -1,11 +1,13 @@
 (function(_) {
-    var ArrayCop = {
+    var array_ = {
         isValid: function(arr) {
             return (Array.isArray(arr)) ? true : console.log(typeof(arr) + " is not an Array");
         },
 
         flatten: function(arr) {
-            // Flattens all the layers of an array.
+            /**
+             **       Flattens all the layers of an array, no matter how deeply nested they are.
+             */
 
             var __ = this;
             return this.isValid(arr) ? arr.reduce(function(f, i) {
@@ -14,9 +16,10 @@
         },
 
         dedup: function(arr) {
-            // Removes duplicates from an array. Note: items in nested arrays are not
-            // considered to be duplicates. If you want to remove duplicates from
-            // the sub-arrays as well then apply flatten() first.
+            /**       Removes duplicates from an array. Note: items in nested arrays are not
+             *        considered to be duplicates. If you want to remove duplicates from
+             *        the sub-arrays as well then apply flatten() first.
+             */
 
             if (this.isValid(arr)) {
                 var newArray = [];
@@ -31,16 +34,36 @@
         },
 
         rand: function(arr, min, max) {
-            // Picks a random items from an array in a range of min, max.
-            // If no range is provided then array.length is taken
-            // into a consideraton
+            /**
+             *        Picks a random item from an array in a range of min, max.
+             *        If no range is provided then array.length is taken
+             *        into a consideraton.
+             */
 
             return this.isValid(arr) ? arr[Math.floor(Math.random() * ((max || arr.length) - (min || 0))) + (min || 0)] : arr;
+        },
+
+        sum: function(arr) {
+            /**
+             *        Returns the sum of all Number items in an array. If an array contains
+             *        NaN items they will be skipped. sum() grabs all Number items from
+             *        nested items as well.
+             */
+
+            if (this.isValid(arr)) {
+                arr = this.flatten(arr);
+                var sum = 0;
+                for (var i in arr) {
+                    typeof(arr[i]) === 'number' ? sum += arr[i]: null;
+                }
+                return sum;
+            }
+            return arr;
         }
     }
     if (typeof module !== 'undefined' && module.exports) {
-        module.exports = ArrayCop;
+        module.exports = array_;
     } else {
-        _.ArrayCop = ArrayCop;
+        _.array_ = array_;
     }
 })(this);
